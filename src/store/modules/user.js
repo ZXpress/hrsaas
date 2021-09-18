01bd0fc1669fd7a25959a63a96de12b86fc88cd9
zcy <1832244088@qq.com>
2021-09-18 10:06:56 +0800

    axios响应拦截器

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,31 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+// 状态
+const state = {
+  token: getToken() // 设置token为共享状态  一初始化先从缓存中读取
+}
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    state.token = token // 数据设置给state
+    setToken(token) // vuex和缓存同步
+  },
+  removeToken(state) {
+    state.token = null // 删除vuextoken
+    removeToken() // 再清空缓存中的tken
+  }
+}
+// 执行异步
+const actions = {
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data) // 拿到token
+    context.commit('setToken', result) // 设置token
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,23 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
-const service = axios.create() // 创建一个axios的实例
+import { Message } from 'element-ui'
+const service = axios.create({
+    baseURL: process.env.VUE_APP_BASE_API,
+    timeout: 5000
+}) // 创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
-service.interceptors.response.use() // 响应拦截器
+service.interceptors.response.use(response => {
+    // axios默认加了一层data
+    const { success, message, data } = response.data
+    // 根据success的成功与否决定下面的操作
+    if (success) {
+        return data
+    } else {
+        Message.error(message) // 提示错误消息
+        return Promise.reject(new Error(message))
+    }
+}, error => {
+    Message.error(error.message) // 提示错误信息
+    return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入catch捕获错误
+}) // 响应拦截器
 export default service // 导出axios实例
